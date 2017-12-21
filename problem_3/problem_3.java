d2daca2294db7b69f9ccbe65fa5b3091f1516dfb
sagautam5 <sagautam5@gmail.com>
2017-12-21 08:34:06 +0545

    problem 3 solved

--- problem_3/problem_3.java
+++ problem_3/problem_3.java
@@ -1,7 +1,31 @@
 import java.util.*;
 
 class problem_3{
-    public static void main(String[] args){
 
+    public static int checkPrime(int num){
+
+        int is_prime = 1;
+        for(int i=2; i<Math.sqrt(num); i++){
+            if(num % i == 0){
+                is_prime = 0;
+            }
+        }
+
+        return is_prime;
+    }
+    public static void main(String[] args){
+        
+        long number = 600851475143L;        
+        int largest = 2;
+
+        for(int i=2; i<Math.sqrt(number); i++){
+            if(number % i == 0){
+                if(checkPrime(i)==1){
+                    largest = i;
+                }
+            }
+        }
+
+        System.out.println("Largest Prime: "+ largest);
     }
 }
